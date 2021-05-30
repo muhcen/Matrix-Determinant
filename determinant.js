13abd2c69573197e8e43d4674d816177c0786d06
muhcen <mohsenun7@gmail.com>
2021-05-30 09:00:50 +0430

    fixed some bad code

--- determinant.js
+++ determinant.js
@@ -5,22 +5,6 @@ function determinant(m) {
     } else if (m.length === 2) {
         const d = m[0][0] * m[1][1] - m[0][1] * m[1][0];
         return d;
-    } else if (m.length === 3) {
-        let newArr = [];
-
-        for (let index = 0; index < m[0].length; index++) {
-            let arr = [];
-            for (let i = 1; i < m.length; i++) {
-                // console.log(m[i].slice(index + 1, m.length + 1));
-                arr.push([...m[i].slice(0, index), ...m[i].slice(index + 1, m.length + 1)]);
-            }
-            newArr.push(arr);
-        }
-        return (
-            m[0][0] * determinant(newArr[0]) -
-            m[0][1] * determinant(newArr[1]) +
-            m[0][2] * determinant(newArr[2])
-        );
     } else {
         let sum = 0;
         let r = -1;
@@ -28,7 +12,6 @@ function determinant(m) {
             let newArr = [];
 
             for (let i = 1; i < m.length; i++) {
-                // console.log(m[i].slice(index + 1, m.length + 1));
                 newArr.push([...m[i].slice(0, index), ...m[i].slice(index + 1, m.length + 1)]);
             }
 
@@ -38,3 +21,15 @@ function determinant(m) {
         return sum;
     }
 }
+
+console.log(
+    determinant([
+        [3, 30, 34, 3, -43, 3, 2],
+        [3, 3, 4, 3, 3, -3, 2],
+        [3, 3, 4, 3, 3, 3, 2],
+        [3, 30, -4, -3, 3, 3, 2],
+        [3, 3, 4, 30, 3, 3, 2],
+        [3, -1, 4, 1, 1, 1, 12],
+        [1, 1, 4, 10, 1, 1, 2],
+    ]),
+);
